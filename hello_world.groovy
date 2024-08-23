3d027a8492dab7063f68d7d674a40dc285deeecd
Rion <rioneffendy0001repo@gmail.com>
2024-08-23 22:24:21 +0700

    Create hello_world.groovy

---
+++ hello_world.groovy
@@ -0,0 +1,8 @@
+// Meminta input dari pengguna
+print "Masukkan jumlah pengulangan: "
+int n = System.console().readLine() as Integer
+
+// Menampilkan "Hello, World!" sebanyak n kali
+n.times {
+    println "Hello, World!"
+}
